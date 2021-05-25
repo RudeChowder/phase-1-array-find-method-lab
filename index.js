f96b83eb3a1d6d673326e8ecb3d37631d45fe8f2
Stephen Crane <scrane88@gmail.com>
2021-05-25 11:59:27 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function superbowlWin(record){
+    function checkForWin(element){
+        return element.result === "W"
+    }
+    
+    const winningYear = record.find(checkForWin)
+    if (typeof winningYear === 'object'){
+        return winningYear.year
+    } else {
+        return undefined
+    }
+}
