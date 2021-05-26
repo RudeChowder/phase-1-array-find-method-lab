80a3c598279a41776dacdb3999e6fedaca7fc925
Stephen Crane <scrane88@gmail.com>
2021-05-26 11:28:52 -0400

    remove commented out code

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 function superbowlWin(record){
-    // function checkForWin(element){
-    //     return element.result === "W"
-    // }
-    
-    // const winningYear = record.find(checkForWin)
-    // if (typeof winningYear === 'object'){
-    //     return winningYear.year
-    // } else {
-    //     return undefined
-
     const winningRecord = record.find( element => element.result === 'W')
     if (winningRecord) {
         return winningRecord.year
